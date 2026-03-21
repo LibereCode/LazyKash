c8c0c874828870a6d854b48f65f1920c9d1f8b13
Libere Code <libere-code@posteo.org>
2026-03-21 13:57:56 +0100

    oneliners

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -111,6 +111,7 @@ return {
     },
   },
 
+  -- NOTE:
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
   { import = "lazyvim.plugins.extras.lang.typescript" },
